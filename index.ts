8957d3ca0454d0dba11ec5142dc1dfc986523d68
PZ <pzika@langmaster.cz>
2017-02-25 11:44:46 +0100

    start

---
+++ index.ts
@@ -0,0 +1,39 @@
+﻿//https://console.firebase.google.com/
+
+import * as fb from 'firebase';
+import * as fela from 'fela';
+import * as felaDOM from 'fela-dom';
+
+export const init = () => {
+  //firebaseTest();
+  felaTest();
+};
+
+const firebaseTest = () => {
+  const config = {
+    apiKey: "AIzaSyAlM8ZQjMGh4-HhGVhTvmYSZp6NaEsQmXI",
+    authDomain: "lm-rw-d4178.firebaseapp.com",
+    databaseURL: "https://lm-rw-d4178.firebaseio.com",
+    storageBucket: "lm-rw-d4178.appspot.com",
+    messagingSenderId: "874593512524"
+  };
+  const app = fb.initializeApp(config, 'db');
+
+  const db = app.database();
+  db.ref('root/data1').set({
+    fld1: 'fld1',
+    fld2: new Date().getTime()
+  });
+};
+
+const felaTest = () => {
+  const renderer = fela.createRenderer();
+
+  const rule: fela.TRule = (props: { size: string; }) => ({
+    backgroundColor: 'red',
+    fontSize: props.size,
+    color: 'blue'
+  })
+
+  renderer.renderRule(rule, { size: '12px' })
+};
---
+++ typings/fela.d.ts
@@ -0,0 +1,44 @@
+﻿declare module "fela" {
+
+  import React from 'react';
+
+  type TProps = {};
+  type TRule = (props: TProps) => IStyle;
+  type TKeyFrame = TRule;
+  type TRendererCreator = (config?: IConfig) => IRenderer;
+  type TPlugin = (style: IStyle) => IStyle; //http://fela.js.org/docs/advanced/Plugins.html
+  type TEnhancer = (renderer: IRenderer) => IRenderer; //http://fela.js.org/docs/advanced/Enhancers.html
+
+  interface IRenderer {
+    renderRule(rule: TRule, props: TProps): void;
+    renderKeyframe(keyFrame: TKeyFrame, props: TProps): void;
+    renderFont(family: string, files: Array<string>, props: TProps): void;
+    renderStatic(style: string, selector?: string): void;
+    renderToString(): string;
+    subscribe(event: (msg: string) => void): { unsubscribe: () => void; }
+    clear();
+  }
+
+  //http://fela.js.org/docs/advanced/RendererConfiguration.html
+  interface IConfig {
+    plugins?: Array<TPlugin>;
+    keyframePrefixes?: Array<string>;
+    enhancers?: Array<TEnhancer>;
+    mediaQueryOrder?: Array<string>;
+    selectorPrefix?: string;
+  }
+
+  interface IStyle extends React.CSSProperties {
+    //TODO: add properties, missing in React CSSProperties
+  }
+
+  function createRenderer(config?: IConfig): IRenderer;
+  function combineRules(...rules: Array<TRule>): TRule;
+  function enhance(...enhancers: Array<TEnhancer>): (rendererCreator: TRendererCreator) => TRendererCreator;
+}
+
+declare module "fela-dom" {
+  import { IRenderer } from 'fela';
+  function render(renderer: IRenderer, node: HTMLElement);
+}
+
